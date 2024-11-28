951801ca08b43dc14bee3f3c236e433c74ede43d
ShahistaDafedar25 <dafedarshahista20@gmail.com>
2024-11-28 20:13:19 +0530

    changes in db file

--- config/db.js
+++ config/db.js
@@ -1,27 +1,11 @@
-const mysql = require('mysql2');
-
-// Check if JAWSDB_URL environment variable is set (Heroku will automatically set this)
-const connectionConfig = process.env.JAWSDB_URL
-    ? {
-        uri: process.env.JAWSDB_URL, // Use Heroku's JawsDB connection string
-      }
-    : {
-        host: 'localhost',   // Localhost for local development
-        user: 'root',        // Your local MySQL user (e.g., 'root' for local development)
-        password: 'Shahista@123',  // Your local MySQL password
-        database: 'health_management_system'  // The name of your local database
-      };
-
-// Create a connection to the database using the appropriate configuration
-const connection = mysql.createConnection(connectionConfig.uri || connectionConfig);
-
-// Connect to the database
+const mysql = require('mysql');
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database as id ' + connection.threadId);
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the MySQL database');
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser'); // Add this line to import body parser
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 // const router = require('./routes/router'); // Import the central router
 // app.use('/', router); // Use the central router
 
